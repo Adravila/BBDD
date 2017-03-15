ca62c986884bb0fb315f0b8d8aca1f218d9bfdfd
adravila <adrian.davilaguerra@gmail.com>
2017-03-15 11:47:50 +0100

    Fichero actualizado

--- Curso_2017/importado.sql
+++ Curso_2017/importado.sql
@@ -10,22 +10,29 @@ CREATE TABLE articulos(
 	PRIMARY KEY(art_num)
 );
 
-INSERT INTO articulos VALUES(1,'impresora',150,'rojo',400,580,4);
-INSERT INTO articulos VALUES(2,'calculadora',150,'negro',4000,4700,1);
-INSERT INTO articulos VALUES(3,'calendario',100,'blanco',420,600,4);
-INSERT INTO articulos VALUES(4,'lampara',550,'rojo',2100,2980,5);
-INSERT INTO articulos VALUES(5,'lampara',550,'blanco',2000,2900,5);
-INSERT INTO articulos VALUES(6,'lampara',550,'azul',2100,2980,5);
-INSERT INTO articulos VALUES(7,'lampara',550,'verde',2100,2980,5);
-INSERT INTO articulos VALUES(8,'pesacartas 1-500',null,null,2400,4000,3);
-INSERT INTO articulos VALUES(9,'pesacartas 1-1000',null,null,3000,5000,3);
-INSERT INTO articulos VALUES(10,'boligrafo', 20,'rojo',20,40,2);
-INSERT INTO articulos VALUES(11,'boligrafo',20,'azul',20,40,2);
-INSERT INTO articulos VALUES(12,'boligrafo lujo',20,'rojo',60,100,2);
-INSERT INTO articulos VALUES(13,'boligrafo lujo',20,'verde',60,100,2);
-INSERT INTO articulos VALUES(14,'boligrafo lujo',20,'azul',60,100,2);
-INSERT INTO articulos VALUES(15,'boligrafo lujo',20,'negro',60,100,2);
-INSERT INTO articulos VALUES(16,'papel',5000,'rosa',3500,4000,2);
+INSERT INTO articulos VALUES(1, 'impresora', 150, 'rojo', 400, 580, 4);
+INSERT INTO articulos VALUES(2, 'calculadora', 150, 'negro', 4000, 4700, 1);
+INSERT INTO articulos VALUES(3, 'calendario', 100, 'blanco', 420, 600, 4);
+INSERT INTO articulos VALUES(4, 'lampara', 550, 'rojo', 2100, 2980, 5);
+INSERT INTO articulos VALUES(5, 'lampara', 550, 'blanco', 2000, 2900, 5);
+INSERT INTO articulos VALUES(6, 'lampara', 550, 'azul', 2100, 2980, 5);
+INSERT INTO articulos VALUES(7, 'lampara', 550, 'verde', 2100, 2980, 5);
+INSERT INTO articulos VALUES(8, 'pesacartas 1-500', NULL, NULL, 2400, 4000, 3);
+INSERT INTO articulos VALUES(9, 'pesacartas 1-1000', NULL, NULL, 3000, 5000, 3);
+INSERT INTO articulos VALUES(10, 'boligrafo', 20, 'rojo', 20, 40, 2);
+INSERT INTO articulos VALUES(11, 'boligrafo', 20, 'azul', 20, 40, 2);
+INSERT INTO articulos VALUES(12, 'boligrafo lujo', 20, 'rojo', 60, 100, 2);
+INSERT INTO articulos VALUES(13, 'boligrafo lujo', 20, 'verde', 60, 100, 2);
+INSERT INTO articulos VALUES(14, 'boligrafo lujo', 20, 'azul', 60, 100, 2);
+INSERT INTO articulos VALUES(15, 'boligrafo lujo', 20, 'negro', 60, 100, 2);
+INSERT INTO articulos VALUES(16, 'papel', 5000, 'rosa', 3500, 4000, 2);
+INSERT INTO articulos VALUES(17, 'coche', 1000, 'verde', 5000, 6000, 3);
+INSERT INTO articulos VALUES(18, 'impresora', 200, 'morado', 400, 800, 3);
+INSERT INTO articulos VALUES(19, 'calendario', 110, 'negro', 420, 600, 4);
+INSERT INTO articulos VALUES(20, 'manta', NULL, 'malva', 400, 600, 10);
+INSERT INTO articulos VALUES(69, 'papel', NULL, 'rosa', 40, 50, NULL);
+INSERT INTO articulos VALUES(99, 'papel', 800, 'morado', 90, 800, 1);
+
 
 DROP TABLE tiendas;
 CREATE TABLE tiendas(
@@ -47,6 +54,7 @@ INSERT INTO tiendas VALUES(9,'palencia','castroviejo, lozano');
 INSERT INTO tiendas VALUES(10,'gerona','gomez, gabriel');
 INSERT INTO tiendas VALUES(11,'lyon','madoux, jean');
 INSERT INTO tiendas VALUES(12,'paris','fouet, paul');
+INSERT INTO tiendas VALUES(13, 'Jerez', 'Messi, Leo');
 
 DROP TABLE clientes;
 CREATE TABLE clientes(
@@ -74,6 +82,16 @@ INSERT INTO clientes VALUES(13,'cortes','diego','e','madrid');
 INSERT INTO clientes VALUES(14,'fernandez','joaquin','e','madrid');
 INSERT INTO clientes VALUES(15,'duran','jacinto','e','pamplona');
 INSERT INTO clientes VALUES(16,'minguin','pedro','e','pamplona');
+INSERT INTO clientes VALUES(17, 'ubrique', 'jesus', 'e', 'pamplona');
+INSERT INTO clientes VALUES(18, 'mazapato', 'sophie', 'e', 'madrid');
+INSERT INTO clientes VALUES(19, 'bigote', 'jose mari', 'p', 'oporto');
+INSERT INTO clientes VALUES(20, 'dalima sorda', 'romualdo', 'b', 'san jose');
+INSERT INTO clientes VALUES(21, 'clavel rojo', 'paco', 'e', 'mala');
+INSERT INTO clientes VALUES(22, 'Alonso', 'Fernando', 'e', 'Gijon');
+INSERT INTO clientes VALUES(23, 'Rodriguez', 'Pedrito', 'e', 'Arico');
+INSERT INTO clientes VALUES(24, 'Florero', 'Mar', 'e', 'Madrid');
+INSERT INTO clientes VALUES(25, 'Florero', 'Mar', 'e', 'Barcelona');
+INSERT INTO clientes VALUES(26, 'Messi', 'Leo', 'a', 'Rosario');
 
 DROP TABLE proveedores;
 CREATE TABLE proveedores(
@@ -87,6 +105,11 @@ INSERT INTO proveedores VALUES(2,'estilografica reunidas');
 INSERT INTO proveedores VALUES(3,'mecanica de precision');
 INSERT INTO proveedores VALUES(4,'sanjita');
 INSERT INTO proveedores VALUES(5,'electrolamp');
+INSERT INTO proveedores VALUES(6, 'copisteria');
+INSERT INTO proveedores VALUES(7, 'el corte ingles');
+INSERT INTO proveedores VALUES(8, 'Mercadiz');
+INSERT INTO proveedores VALUES(9, 'El ahorramas');
+INSERT INTO proveedores VALUES(10, 'zara hogar');
 
 DROP TABLE ventas;
 CREATE TABLE ventas(
@@ -101,22 +124,39 @@ CREATE TABLE ventas(
 	FOREIGN KEY (vnt_tda) REFERENCES tiendas (tda_num)
 );
 
-INSERT INTO ventas VALUES(5,4,4,1,2980,910106);
-INSERT INTO ventas VALUES(7,3,10,1,40,910106);
-INSERT INTO ventas VALUES(7,3,11,2,80,910106);
-INSERT INTO ventas VALUES(7,3,14,3,300,910106);
-INSERT INTO ventas VALUES(8,11,2,1,4700,910109);
-INSERT INTO ventas VALUES(6,12,3,2,1200,910109);
-INSERT INTO ventas VALUES(6,12,15,2,200,910109);
-INSERT INTO ventas VALUES(13,1,4,1,2980,910109);
-INSERT INTO ventas VALUES(13,1,3,1,600,910110);
-INSERT INTO ventas VALUES(1,2,2,1,4700,910110);
-INSERT INTO ventas VALUES(1,2,12,1,100,910110);
-INSERT INTO ventas VALUES(1,2,13,10,1000,910110);
-INSERT INTO ventas VALUES(4,11,1,8,4640,910111);
-INSERT INTO ventas VALUES(4,11,10,7,280,910111);
-INSERT INTO ventas VALUES(3,7,6,1,2980,910111);
-INSERT INTO ventas VALUES(3,7,9,2,10000,910111);
+INSERT INTO ventas VALUES(22, 10, 17, 1, 6000, '991231');
+INSERT INTO ventas VALUES(5, 4, 4, 1, 2980, '910106');
+INSERT INTO ventas VALUES(7, 3, 10, 1, 40, '910106');
+INSERT INTO ventas VALUES(7, 3, 11, 2, 80, '910106');
+INSERT INTO ventas VALUES(7, 3, 14, 3, 300, '910106');
+INSERT INTO ventas VALUES(8, 11, 2, 1, 4700, '910109');
+INSERT INTO ventas VALUES(6, 12, 3, 2, 1200, '910109');
+INSERT INTO ventas VALUES(6, 12, 15, 2, 200, '910109');
+INSERT INTO ventas VALUES(13, 1, 4, 1, 2980, '910109');
+INSERT INTO ventas VALUES(13, 1, 3, 1, 600, '910110');
+INSERT INTO ventas VALUES(1, 2, 2, 1, 4700, '910110');
+INSERT INTO ventas VALUES(1, 2, 12, 1, 100, '910110');
+INSERT INTO ventas VALUES(1, 2, 13, 10, 1000, '910110');
+INSERT INTO ventas VALUES(4, 11, 1, 8, 4640, '910111');
+INSERT INTO ventas VALUES(4, 11, 10, 7, 280, '910111');
+INSERT INTO ventas VALUES(3, 7, 6, 1, 2980, '910111');
+INSERT INTO ventas VALUES(3, 7, 9, 2, 10000, '910111');
+INSERT INTO ventas VALUES(1, 2, 3, 3, 1800, '910120');
+INSERT INTO ventas VALUES(7, 8, 3, 1, 600, '910203');
+INSERT INTO ventas VALUES(4, 5, 3, 6, 3000, '910114');
+INSERT INTO ventas VALUES(10, 11, 3, 1, 600, '910116');
+INSERT INTO ventas VALUES(6, 7, 3, 1, 600, '910111');
+INSERT INTO ventas VALUES(3, 4, 3, 2, 1200, '910206');
+INSERT INTO ventas VALUES(9, 10, 3, 1, 600, '910201');
+INSERT INTO ventas VALUES(2, 3, 3, 4, 2400, '910102');
+INSERT INTO ventas VALUES(8, 9, 3, 1, 500, '910130');
+INSERT INTO ventas VALUES(5, 6, 3, 3, 1800, '910116');
+INSERT INTO ventas VALUES(26, 4, 17, 2, 12000, '990101');
+INSERT INTO ventas VALUES(19, 7, 3, 1, NULL, '960601');
+INSERT INTO ventas VALUES(17, 4, 3, 10, NULL, '961028');
+INSERT INTO ventas VALUES(18, 1, 3, 1, NULL, '961130');
+INSERT INTO ventas VALUES(10, 4, 2, NULL, NULL, '920822');
+INSERT INTO ventas VALUES(3, 13, 3, NULL, NULL, '940430');
 
 DROP TABLE pesos;
 CREATE TABLE pesos(
@@ -130,3 +170,4 @@ INSERT INTO pesos VALUES('leve',0,100);
 INSERT INTO pesos VALUES('ligero',101,500);
 INSERT INTO pesos VALUES('medio',501,2500);
 INSERT INTO pesos VALUES('pesado',2501,9999);
+INSERT INTO pesos VALUES('sin peso', 0, 0);
