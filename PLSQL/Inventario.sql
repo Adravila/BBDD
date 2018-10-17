c4080b2c913ed8fd1076812e127ba6683e101623
Adrián <Adravila@users.noreply.github.com>
2018-10-17 20:25:07 +0200

    Update Inventario.sql

--- PLSQL/Inventario.sql
+++ PLSQL/Inventario.sql
@@ -1,16 +1,19 @@
-/* Autor: Adrián Dávila Guerra 
-*  Práctica 1.3
-*/
+/* Autor: Adravila */
 
 SET SERVEROUTPUT ON
 
 DECLARE
 	v_reg_inventario inventario%ROWTYPE;
 	v_reg_controlventas controlventas%ROWTYPE;
+
 	cod_art_ INTEGER;
+	cantidad_ INTEGER;
 BEGIN
 	/* Se introducirá el código del artículo para que procese el orden de compra */
 	cod_art_ := &cod_art;
+
+	/* Se introducirá la cantidad solicita */
+	cantidad_ := &cantidad;
 
 	/* Hacemos una recuperación para obtener la fila de dicho artículo  */
 	SELECT * INTO v_reg_inventario
@@ -23,12 +26,14 @@ BEGIN
 
 	DBMS_OUTPUT.PUT_LINE(CHR(8)); -- Retorno de carro
 
-	IF v_reg_inventario.stock_art >= v_reg_controlventas.num_unidades AND nvl(v_reg_controlventas.comentario,'Nulo') = 'Nulo' THEN
+	IF v_reg_inventario.stock_art >= cantidad_ THEN
 		DBMS_OUTPUT.PUT_LINE('Procede el articulo: ' || v_reg_inventario.nombre_art);
 		-- Actualizar el stock del articulo del inventario
-		v_reg_inventario.stock_art := v_reg_inventario.stock_art - v_reg_controlventas.num_unidades;
+		v_reg_inventario.stock_art := v_reg_inventario.stock_art - cantidad_;
 		-- Actualizar la fecha de la ultima modificacion del articulo
 		v_reg_inventario.fecha :=  SYSDATE;
+		-- Actualizar el número de unidades vendidas
+		v_reg_controlventas.num_unidades :=  cantidad_;
 		-- Actualizar la fecha de la ultima solicitud (control ventas)
 		v_reg_controlventas.fecha :=  SYSDATE;
 		-- Actualizar comentario indicando que la venta ha sido realizada
@@ -37,17 +42,16 @@ BEGIN
 		UPDATE inventario SET row = v_reg_inventario WHERE cod_art = v_reg_inventario.cod_art;
 		UPDATE controlventas SET row = v_reg_controlventas WHERE cod_art = v_reg_controlventas.cod_art;
 
-	ELSIF v_reg_inventario.stock_art < v_reg_controlventas.num_unidades THEN
+	ELSIF v_reg_inventario.stock_art < cantidad_ THEN
 		DBMS_OUTPUT.PUT_LINE('No hay Stock');
 		-- Actualizar comentario indicando que no hay stock suficiente en el articulo del inventario
 		v_reg_controlventas.comentario := 'No hay stock suficiente';
 		-- Actualizar la fecha de la ultima solicitud (control ventas)
 		v_reg_controlventas.fecha :=  SYSDATE;
+		-- Actualizar el número de unidades solicitadas
+		v_reg_controlventas.num_unidades :=  cantidad_;
 
 		UPDATE controlventas SET row = v_reg_controlventas WHERE cod_art = v_reg_controlventas.cod_art;
-	
-	ELSIF v_reg_controlventas.comentario = 'Venta realizada' THEN
-		DBMS_OUTPUT.PUT_LINE('El articulo ya ha sido procedido.');
 
 	END IF;
 
