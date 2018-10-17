f2adcd053d5b2cd1d255019360ec12975b78b524
Adrián <Adravila@users.noreply.github.com>
2018-10-17 20:25:36 +0200

    Update temperatura.sql

--- PLSQL/temperatura.sql
+++ PLSQL/temperatura.sql
@@ -1,6 +1,4 @@
-/* Autor: Adrián Dávila Guerra 
-*  Práctica 1.1
-*/
+/* Autor: Adravila */
 
 SET SERVEROUTPUT ON
 
@@ -11,9 +9,9 @@ BEGIN
 	temperatura:= &temperatura;
 	escala:= '&escala';
 	
-	IF escala = 'C' THEN
+	IF escala = 'C' || escala = 'c' THEN
 		DBMS_OUTPUT.PUT_LINE('Mostrando temperatura en grados Celsius: ' || (5/9)*(temperatura-32));
-	ELSIF escala = 'F' THEN
+	ELSIF escala = 'F' || escala = 'f' THEN
 		DBMS_OUTPUT.PUT_LINE('Mostrando temperatura en grados Fahrenheit: ' || (9/5)*(temperatura-32));
 	ELSE
 		DBMS_OUTPUT.PUT_LINE('Error - Tienes que introducir C o F');
