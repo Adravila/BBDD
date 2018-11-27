9870095bbb05ede65c4d97a6b6e1dc073df8cf4a
IndieCoolPlanet <45273249+IndieCoolPlanet@users.noreply.github.com>
2018-11-27 20:50:53 +0100

    Update trigg_clase_colectiva.sql

--- ProyectoTABD/Disparadores/trigg_clase_colectiva.sql
+++ ProyectoTABD/Disparadores/trigg_clase_colectiva.sql
@@ -1,8 +1,6 @@
 /**
 Clases colectivas:
-- La fecha no puede ser menor que el día actual (fecha del sistema)
 - La duración no puede ser mayor de 45 minutos.
-- El tipo de clase tiene que ser algunos de estos tipos: {Ciclo, Step, Zumba, Body Pump, ABD, Body Combat, Yoga, Pilates, Body Balance}.
 **/
 
 SET SERVEROUTPUT ON
@@ -16,9 +14,6 @@ DECLARE
 BEGIN
 	IF(:NEW.duracion < 0 || :NEW.duracion > 45) THEN
 		RAISE_APPLICATION_ERROR (-20600,'Error, la duración debe ser menor o igual que 45 minutos.');
-	ELSIF(:NEW.tipos < 0 || :NEW.tipos > 10) THEN
-		RAISE_APPLICATION_ERROR (-20600,'Error, no se corresponde a ninguno de estos tipos.');
-	ELSIF() THEN
-		RAISE_APPLICATION_ERROR(-20600,'Error, la fecha indicada no es válida.');
+	END IF;
 END;
 /
